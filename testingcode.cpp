577a0c9b411aa89989aa916e2306c5b57320bd60
Ashton221 <ashtonmoscoso221@gmail.com>
2023-12-04 12:58:37 -0500

    Add loops for concatenation and Output

--- testingcode.cpp
+++ testingcode.cpp
@@ -5,9 +5,22 @@ using namespace std;
 
 int main(){
 
-  vector<int> nums = {1 , 2 , 1};
+  vector<int> nums{1, 2, 1};
   vector<int> ans;
   ans = nums;
+
+
+// Concatenates two nums arrays
+  for( int i = 0; i < nums.size(); i++)
+  {
+    ans.push_back(nums[i]);
+  }
+
+//Displays ans Array
+  for( int i = 0; i < ans.size(); i++)
+  {
+    cout << ans[i] << endl;
+  }
 
   return 0;
 }
